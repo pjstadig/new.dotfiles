9a87d079071b0340d8682da3ced976a9c0e1e99d
Paul Stadig <paul@stadig.name>
2012-09-15 23:24:29 +0000

    Add emacs support for TERM=screen.

---
+++ .emacs.d/term/screen.el
@@ -0,0 +1,6 @@
+(defun terminal-init-screen ()
+  "Terminal initialization function for screen."
+  ;; Use the xterm color initialization code.
+  (load "term/xterm")
+  (xterm-register-default-colors)
+  (tty-set-up-initial-frame-faces))
