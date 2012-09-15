60d34c715336bb0bf6b5b4b5280079a23d1c7d0e
Paul Stadig <paul@stadig.name>
2012-09-15 11:06:58 +0000

    Setup erc.

--- .emacs.d/paul.el
+++ .emacs.d/paul.el
@@ -65,3 +65,56 @@
 (setq org-mobile-files nil)
 (setq org-mobile-inbox-for-pull (concat home-dir "/org/inbox.org"))
 (setq org-mobile-force-id-on-agenda-items nil)
+
+;; ERC
+(add-hook 'erc-mode-hook '(lambda ()
+                            (erc-scrolltobottom-mode t)))
+
+(eval-after-load 'erc
+  '(progn
+     (setq erc-prompt ">"
+           erc-fill-column 75
+           erc-hide-list '("JOIN" "PART" "QUIT" "NICK")
+           erc-track-exclude-types (append '("324" "329" "332" "333"
+                                             "353" "477" "MODE")
+                                           erc-hide-list)
+           erc-nick "pjstadig"
+           erc-autojoin-timing :ident
+           erc-autojoin-channels-alist
+           '(("freenode.net" "#clojure")
+             ("irc.sa2s.us" "#safe" "#devs"))
+           erc-ignore-list '("sexpbot")
+           erc-prompt-for-nickserv-password nil)
+     (require 'erc-services)
+     (require 'erc-spelling)
+     (erc-services-mode 1)
+     (add-to-list 'erc-modules 'highlight-nicknames 'spelling)
+     (add-hook 'erc-connect-pre-hook (lambda (x) (erc-update-modules)))
+     (set-face-foreground 'erc-input-face "dim gray")
+     (set-face-foreground 'erc-my-nick-face "blue")))
+
+(ignore-errors
+  (load (expand-file-name "~/.passwords.el"))
+  (setq erc-nickserv-passwords
+        `((freenode     (("pjstadig" . ,freenode-password))))))
+
+(defun clean-message (s)
+  (setq s (replace-regexp-in-string "&" "&amp;" s))
+  (setq s (replace-regexp-in-string "'" "&apos;" s))
+  (setq s (replace-regexp-in-string "\"" "&quot;" s))
+  (setq s (replace-regexp-in-string "<" "&lt;" s))
+  (setq s (replace-regexp-in-string ">" "&gt;" s)))
+
+(defun call-libnotify (matched-type nick msg)
+  (let* ((cmsg  (split-string (clean-message msg)))
+         (nick   (first (split-string nick "!")))
+         (msg    (mapconcat 'identity (if (string= (first cmsg) "pjstadig:")
+                                          (rest cmsg)
+                                        cmsg)
+                            " ")))
+    (shell-command-to-string
+     (format "erc-notify '%s says:' '%s'"
+             nick msg))))
+
+(add-hook 'erc-text-matched-hook 'call-libnotify)
+(setq pcomplete-cycle-completions nil)
